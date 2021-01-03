a4b76028ed75192d0ce41bb47f216bc95712effb
Yaroslav Holota <korova2321@gmail.com>
2021-01-03 20:21:54 +0200

    Added difficulty parser

---
+++ WinFormsMineSweeper/IDifficultiesParser.cs
@@ -0,0 +1,9 @@
+﻿using WinFormsMineSweeper.Game;
+
+namespace WinFormsMineSweeper
+{
+    public interface IDifficultiesParser
+    {
+        GameSettings ParseDifficulty(string name);
+    }
+}
---
+++ WinFormsMineSweeper/MockDifficultyParser.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using WinFormsMineSweeper.Game;
+
+namespace WinFormsMineSweeper
+{
+    public class MockDifficultyParser : IDifficultiesParser
+    {
+        public GameSettings ParseDifficulty(string name)
+        {
+            //Move to json document, that would later be parsed
+            switch (name)
+            {
+                case "Easy":
+                    return new GameSettings { Height = 8, Width = 8, MineCount = 10 };
+                case "Medium":
+                    return new GameSettings { Height = 12, Width = 12, MineCount = 30 };
+                case "Hard":
+                    return new GameSettings { Height = 16, Width = 16, MineCount = 50 };
+                default:
+                    throw new UnavailableDifficultyException("Unevailable difficulty");
+            }
+        }
+    }
+}
